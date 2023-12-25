adc57eb8cdc5566217e4f3ed7c1c6b24296ea2d1
adamsh11j <154805689+adamsh11j@users.noreply.github.com>
2023-12-25 12:25:46 +0100

    Update load.ps1

--- load.ps1
+++ load.ps1
@@ -1,2 +1,1 @@
-Invoke-WebRequest -Uri https://github.com/pal1000/mesa-dist-win/releases/download/23.3.1/mesa3d-23.3.1-release-msvc.7z -Outfile $userprofile\Downloads\driver.7z
-Invoke-WebRequest -Uri https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe -Outfile $userprofile\Downloads\steam.exe
+echo none
