0a3652941ed8d72a3cc2ac66fbd086ee2abaafee
adamsh11j <154805689+adamsh11j@users.noreply.github.com>
2023-12-28 17:28:14 +0200

    Update load.ps1

--- load.ps1
+++ load.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri https://github.com/pal1000/mesa-dist-win/releases/download/23.3.1/mesa3d-23.3.1-release-msvc.7z --OutFile $ENV:userprofile\Downloads
+Invoke-WebRequest -Uri https://github.com/pal1000/mesa-dist-win/releases/download/23.3.1/mesa3d-23.3.1-release-msvc.7z -OutFile $ENV:userprofile\Downloads
