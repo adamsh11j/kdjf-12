3439aceea5963c58e5575cc2558861c10296337a
adamsh11j <154805689+adamsh11j@users.noreply.github.com>
2023-12-31 17:40:32 +0200

    Update load.ps1

--- load.ps1
+++ load.ps1
@@ -1,4 +1,5 @@
-choco install warp -y
-Invoke-WebRequest -Uri "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" -OutFile $ENV:userprofile\Downloads\Steam.exe
+choco install wget -y
+choco install windscribe -y
+wget.exe "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" -O "$ENV:userprofile\Downloads\SteamSetup.exe"
 Start-Process -FilePath "$ENV:userprofile\Downloads\SteamSetup.exe" -ArgumentList "/S", "/D=C:\\Steam"
-Invoke-WebRequest -Uri "https://github.com/pal1000/mesa-dist-win/releases/download/23.3.2/mesa3d-23.3.2-release-msvc.7z" -OutFile $ENV:userprofile\Downloads\mesa3d.7z
+wget.exe "https://github.com/pal1000/mesa-dist-win/releases/download/23.3.2/mesa3d-23.3.2-release-msvc.7z" -O "$ENV:userprofile\Downloads\mesa3d.7z"
