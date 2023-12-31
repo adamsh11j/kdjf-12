681bddf54ca96d23a977af31d3fb1cbe009b4aee
adamsh11j <154805689+adamsh11j@users.noreply.github.com>
2023-12-31 17:24:07 +0200

    Update load.ps1

--- load.ps1
+++ load.ps1
@@ -1,1 +1,4 @@
-echo Enter here your depends.
+choco install warp -y
+Invoke-Request -Uri "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" --OutFile $ENV:userprofile\Downloads\Steam.exe
+SteamSetup.exe /S /D=C:\\Steam
+Invoke-Request -Uri "https://github.com/pal1000/mesa-dist-win/releases/download/23.3.2/mesa3d-23.3.2-release-msvc.7z" --OutFile $ENV:userprofile\Downloads\mesa3d.7z
